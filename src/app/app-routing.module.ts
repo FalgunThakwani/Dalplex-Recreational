38ee5e2c162d11ddc8ba8fc209570ff402ce593c
VenkataVijayM <124217062+VenkataVijayM@users.noreply.github.com>
2023-02-26 19:19:58 -0400

    moved to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FacilitiesComponent } from './components/facilities/facilities.component';
-import { TournamentleaguesComponent } from './components/tournamentleagues/tournamentleagues.component';
-import { TournamentmatchesComponent } from './components/tournamentmatches/tournamentmatches.component';
+import { FacilitiesComponent } from './pages/facilities/facilities.component';
+import { TournamentleaguesComponent } from './pages/tournamentleagues/tournamentleagues.component';
+import { TournamentmatchesComponent } from './pages/tournamentmatches/tournamentmatches.component';
 import { TournamentrankingsComponent } from './components/tournamentrankings/tournamentrankings.component';
-import { TournamentfindteamComponent } from './components/tournamentfindteam/tournamentfindteam.component';
-import { TournamentsComponent } from './components/tournaments/tournaments.component';
-import { FacilitycalendarComponent } from './components/facilitycalendar/facilitycalendar.component';
+import { TournamentfindteamComponent } from './pages/tournamentfindteam/tournamentfindteam.component';
+import { TournamentsComponent } from './pages/tournaments/tournaments.component';
+import { FacilitycalendarComponent } from './pages/facilitycalendar/facilitycalendar.component';
 
 const routes: Routes = [
   { path: 'facilities', component: FacilitiesComponent },
