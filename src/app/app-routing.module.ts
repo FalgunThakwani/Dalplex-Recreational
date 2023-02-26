8e23527a5f330f66b1cd22656c5ec8a504d4876e
Sumit Kumar <sm585558@dal.ca>
2023-02-26 17:31:06 -0400

    update imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { PaymentPageComponent } from './pages/payment-page/payment-page.componen
 import { PaymentCompleteComponent } from './pages/payment-complete/payment-complete.component';
 
 import { UserLandingPageComponent } from './pages/user-landing-page/user-landing-page.component';
-import { TournamentsComponent } from './components/tournaments/tournaments.component';
+import { TournamentsComponent } from './pages/tournaments/tournaments.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { SportsCategoryComponent } from './pages/sports-category/sports-category.component';
 import { SportsCategoryDetailComponent } from './pages/sports-category-detail/sports-category-detail.component';
@@ -14,11 +14,11 @@ import { UserLoginComponent } from './pages/user-login/user-login.component';
 import { UserRegistrationComponent } from './pages/user-registration/user-registration.component';
 import { UpcomingBookingsComponent } from './pages/upcoming-bookings/upcoming-bookings.component';
 import { FacilitiesComponent } from './pages/facilities/facilities.component';
-import { TournamentrankingsComponent } from './components/tournamentrankings/tournamentrankings.component';
-import { TournamentmatchesComponent } from './components/tournamentmatches/tournamentmatches.component';
-import { TournamentleaguesComponent } from './components/tournamentleagues/tournamentleagues.component';
-import { TournamentfindteamComponent } from './components/tournamentfindteam/tournamentfindteam.component';
-import { FacilitycalendarComponent } from './components/facilitycalendar/facilitycalendar.component';
+import { TournamentrankingsComponent } from './pages/tournamentrankings/tournamentrankings.component';
+import { TournamentmatchesComponent } from './pages/tournamentmatches/tournamentmatches.component';
+import { TournamentleaguesComponent } from './pages/tournamentleagues/tournamentleagues.component';
+import { TournamentfindteamComponent } from './pages/tournamentfindteam/tournamentfindteam.component';
+import { FacilitycalendarComponent } from './pages/facilitycalendar/facilitycalendar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/landing', pathMatch: 'full' },
