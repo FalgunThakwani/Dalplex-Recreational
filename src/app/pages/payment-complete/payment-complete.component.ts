1e33d3606933c2d0fa1b67e4b8c7cff2778e771b
Falgun Jairaj Thakwani <fl700637@dal.ca>
2023-02-26 22:54:21 -0400

    Updated payment success UI

--- src/app/pages/payment-complete/payment-complete.component.ts
+++ src/app/pages/payment-complete/payment-complete.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment-complete.component.css']
 })
 export class PaymentCompleteComponent implements OnInit {
+  username = "Falgun";
+  bookings= [{name:"Falgun Jairaj Thakwani",time:"7:00 am to 8:00 am",date:"Mar 26, 2023",sport:"BasketBall Court"},
+  {name:"Falgun Jairaj Thakwani",time:"8:30 am to 9:00 am",date:"Mar 26, 2023",sport:"Swimming"}]
+
 
   constructor(private coreService: CoreService) { }
 
