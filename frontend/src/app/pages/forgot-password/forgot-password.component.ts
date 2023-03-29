0df1c5adec23036d7f58b066c59325f145814e64
Vinayak Abhinav Rupanagunta <vn958266@dal.ca>
2023-03-29 16:16:54 -0300

    forgot password otp feature added

--- frontend/src/app/pages/forgot-password/forgot-password.component.ts
+++ frontend/src/app/pages/forgot-password/forgot-password.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CoreService } from 'src/app/services/core.service';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -16,7 +17,7 @@ export class ForgotPasswordComponent implements OnInit {
   newPassword!: String;
   confirmPassword!: String;
 
-  constructor(private router: Router, private service: CoreService, private otpService: OtpService) { }
+  constructor(private router: Router, private service: CoreService, private otpService: OtpService,private userService: UserService) { }
 
   ngOnInit(): void {
     this.PasswordResetForm = new FormGroup({
@@ -28,10 +29,20 @@ export class ForgotPasswordComponent implements OnInit {
   }
 
   onResetPassword() {
+    const email=this.PasswordResetForm.value.email
+    const password=this.PasswordResetForm.value.newPassword
+    const otp=this.PasswordResetForm.value.otp
     if (this.PasswordResetForm.value.newPassword == this.PasswordResetForm.value.confirmPassword &&
       this.PasswordResetForm.value.newPassword != '' && this.PasswordResetForm.value.confirmPassword != '') {
-      this.service.showSnackBar("password reset successful", "password reset successful")
-      this.router.navigateByUrl('/login')
+        this.userService.forgotpassword({email: email,password: password,OTP: otp}).subscribe((data:any)=>{
+          if(data.message=='OK'){
+            this.service.showSnackBar("password reset successful", "password reset successful")
+            this.router.navigateByUrl('/login')
+          }
+          else{
+            this.service.showSnackBar("password reset failed", "password reset failed")
+          }
+        })
     }
   }
 
