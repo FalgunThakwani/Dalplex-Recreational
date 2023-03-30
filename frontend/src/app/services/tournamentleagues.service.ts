e177bc00ac89975e96db5738acc06cea863d3178
VenkataVijayM <124217062+VenkataVijayM@users.noreply.github.com>
2023-03-29 21:14:24 -0300

    tournament matches be added

---
+++ frontend/src/app/services/tournamentleagues.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TournamentleaguesService {
+
+  tournamentLeaguesApiUrl: string = environment.apiServer + "/tleague";
+
+  constructor(private http: HttpClient) { }
+
+  getHeader() {
+    const options = {
+      headers: new HttpHeaders({
+        'Authorization': localStorage.getItem('sessiontoken') ?? ''
+      })
+    };
+    return options;
+  }
+
+  getAlltournamentLeagues() : Observable<any>{
+    return this.http.get(this.tournamentLeaguesApiUrl, this.getHeader());
+  }
+}
